bda6a76f2e2e09eeb98f132d1360d9970583bb3d
Gabrielhzt <hazout26@gmail.com>
2024-04-15 13:50:50 +0200

    Added the login and register with passport jwt

--- server/server.js
+++ server/server.js
@@ -4,11 +4,14 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 3000;
+const authRoutes = require('./Routes/auth');
 
 app.use(cors());
   
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use('/auth', authRoutes);
 
 app.listen(port, () => {
     console.log(`Server is listening at port: ${port}`);
