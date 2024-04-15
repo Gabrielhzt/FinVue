f51bba3424049f7c1dae541861b92cf8baf335a6
Gabrielhzt <hazout26@gmail.com>
2024-04-15 17:34:41 +0200

    Added the incomesRoutes

---
+++ server/Routes/incomes.js
@@ -0,0 +1,91 @@
+const express = require('express');
+const router = express.Router();
+const pool = require('../database');
+
+router.get('/', (req, res) => {
+    const userId = req.user.user_id;
+
+    pool.query('SELECT * FROM incomes WHERE user_id = $1', [userId], (error, result) => {
+        if(error) {
+            res.status(500).send('Error');
+        }else {
+            res.send(result.rows)
+        }
+    })
+})
+
+router.post('/add', (req, res) => {
+    const userId = req.user.user_id;
+    const { type, title, amount, date, member } = req.body;
+
+    pool.query('INSERT INTO incomes (user_id, type, title, amount, date, member) VALUES ($1, $2, $3, $4, $5)', [userId, type, title, amount, date, member], (error, result) => {
+        if(error) {
+            res.status(500).send('Error adding income');
+        }else {
+            res.send('Added successfully')
+        }
+    })
+})
+
+router.put('/update/:incomeId', (req, res) => {
+    const { incomeId } = req.params;
+    const { type, title, amount, date, member } = req.body;
+
+    pool.query('UPDATE incomes SET type = $1, title = $2, amount = $3, date = $4, member = $5 WHERE income_id = $6', [type, title, amount, date, member, incomeId], (error, result) => {
+        if(error) {
+            res.status(500).send('Error updating income');
+        }else {
+            res.send('Added successfully')
+        }
+    })
+})
+
+router.delete('/delete/:incomeId', (req, res) => {
+    const { incomeId } = req.params;
+
+    pool.query('DELETE FROM incomes WHERE income_id = $1', [incomeId], (error, result) => {
+        if(error) {
+            res.status(500).send('Error deleting income');
+        }else {
+            res.send('Successfully deleting income')
+        }
+    })
+})
+
+router.get('/allfilter', (req, res) => {
+    const userId = req.user.user_id;
+
+    pool.query('SELECT type, SUM(amount) AS total_amount FROM incomes WHERE user_id = $1 GROUP BY type', [userId], (error, result) => {
+        if(error) {
+            res.status(500).send('Error');
+        }else {
+            res.send(result.rows)
+        }
+    })
+})
+
+router.get('/filter', (req, res) => {
+    const { type } = req.body;
+
+    pool.query('SELECT * FROM incomes WHERE type = $1', [type], (error, result) => {
+        if(error) {
+            res.status(500).send('Error');
+        }else {
+            res.send(result.rows)
+        }
+    })
+})
+
+router.get('/total', (req, res) => {
+    const userId = req.user.user_id;
+
+    pool.query('SELECT EXTRACT(MONTH FROM date) AS month, SUM(amount) AS total_income FROM incomes WHERE user_id = $1 GROUP BY EXTRACT(MONTH FROM date) ORDER BY month;', [userId], (error, result) => {
+        if(error) {
+            res.status(500).send('Error')
+        }else {
+            res.send(result.rows)
+        }
+    })
+})
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -5,6 +5,8 @@ const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 3000;
 const authRoutes = require('./Routes/auth');
+const incomesRoutes = require('./Routes/incomes');
+const passport = require('passport');
 
 app.use(cors());
   
@@ -12,6 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/auth', authRoutes);
+app.use('/incomes', passport.authenticate('jwt', { session: false }), incomesRoutes);
 
 app.listen(port, () => {
     console.log(`Server is listening at port: ${port}`);
